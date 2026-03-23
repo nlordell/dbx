26f172dd393ec3408312e7bcd9cdf04a666c1f29
Nicholas Rodrigues Lordello <n@lordello.net>
2026-03-23 10:22:06 +0100

    Link Devbox Commands and Proxy TCP Connections
    Improve DX by adding a utility to create linked executables that run
    in a devbox (for example, editors can natively run LSP servers) and
    proxy container TCP connections to the host.
    
    The proxying works over stdin and stdout, so it does not require the
    container to publish ports (and know what ports need to be published
    beforehand). It was written in Go (since writing this in Bash was too
    unruly).
    
    I want to port the whole project to OCaml once I have time :)

---
+++ dbx-proxy.go
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: 0BSD
+package main
+
+import (
+	"bufio"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+)
+
+const C string = "dbx"
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("ERROR: invalid command arguments")
+	}
+
+	port := os.Args[1]
+	addr := "localhost:" + port
+	conn, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	for {
+		client, err := conn.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleClient(client, port)
+	}
+}
+
+func handleClient(client net.Conn, port string) error {
+	log.Print("connected ", client.RemoteAddr())
+	defer client.Close()
+
+	netcat := exec.Command("podman", "exec", "--interactive", "--user=1000", C, "netcat", "-N", "localhost", port)
+	netcat.Stdin = client
+	netcat.Stdout = client
+
+	stderr, err := netcat.StderrPipe()
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	defer stderr.Close()
+
+	if err := netcat.Start(); err != nil {
+		log.Print(err)
+		return err
+	}
+
+	scanner := bufio.NewScanner(stderr)
+	for scanner.Scan() {
+		log.Print("netcat 2>", scanner.Text())
+	}
+
+	client.Close()
+	if err := netcat.Wait(); err != nil {
+		log.Print(err)
+		return err
+	}
+
+	log.Print("disconnected ", client.RemoteAddr())
+	return nil
+}
